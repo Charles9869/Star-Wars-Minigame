2bc0b4a24a62561ca2294ab099a59952f3fd7cb5
Charles Bertrand <cbertrand2198@gmail.com>
2020-05-28 14:05:10 -0400

    added touchscreen support

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const startBtn = document.querySelector('.btn');
 const mainLogo = document.getElementById('logo');
 
 const gameEngine = new Engine(document.getElementById('app'));
-const keydownHandler = event => {
+const keydownHandler = (event) => {
   if (gameStarted === true) {
     if (event.code === 'ArrowLeft') gameEngine.player.moveLeft();
     if (event.code === 'ArrowRight') gameEngine.player.moveRight();
@@ -16,18 +16,24 @@ const keydownHandler = event => {
 
 document.addEventListener('keydown', keydownHandler);
 
+// Support touchscreen on movile devices
+document.addEventListener('touchstart', (e) => {
+  if (e.touches[0].pageX < GAME_WIDTH / 2) gameEngine.player.moveLeft();
+  else gameEngine.player.moveRight();
+});
+
 const startGame = () => {
   gameStarted = true;
   startBtn.style.display = mainLogo.style.display = 'none';
   if (gameStarted) gameEngine.gameLoop();
 };
 
-const hide = object => (object.domElement.style.display = 'none');
+const hide = (object) => (object.domElement.style.display = 'none');
 
-const show = object => (object.domElement.style.display = 'block');
+const show = (object) => (object.domElement.style.display = 'block');
 
 const emptyArray = (root, objects) => {
-  objects.forEach(object => {
+  objects.forEach((object) => {
     root.removeChild(object.domElement);
     object = null;
   });
